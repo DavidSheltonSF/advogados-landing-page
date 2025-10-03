ce32c3f2dda8acc0459cd45a2ee0fd1fa3001665
DavidSheltonSF <david.faria012@gmail.com>
2025-10-03 19:35:58 -0300

    fix: fix whatsapp number

--- src/app/components/whatsappButton/index.tsx
+++ src/app/components/whatsappButton/index.tsx
@@ -2,7 +2,7 @@ import { FaWhatsapp } from "react-icons/fa"
 
 export function WhatsAppButton() {
   return (
-    <a href="https://wa.me/21974694530"  target="_blank" className="fixed button-5 right-5 bottom-8 bg-green-500 hover:bg-green-600 rounded-full p-4 shadow-lg transition duration-300 z-50">
+    <a href="https://wa.me/+5521974694530"  target="_blank" className="fixed button-5 right-5 bottom-8 bg-green-500 hover:bg-green-600 rounded-full p-4 shadow-lg transition duration-300 z-50">
       <FaWhatsapp size={32}/>
     </a>
   )
