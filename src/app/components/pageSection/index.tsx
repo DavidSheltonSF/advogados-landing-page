78b5bb0e71e963bff74d79db3ca3797c5c5812e3
DavidSheltonSF <david.faria012@gmail.com>
2025-10-03 18:28:52 -0300

    feat(components): add mobileConfigClasses property to PageSection

--- src/app/components/pageSection/index.tsx
+++ src/app/components/pageSection/index.tsx
@@ -2,14 +2,15 @@ import { ReactNode } from "react";
 
 interface Props {
   backgroundImage: string,
-  children?: any
+  children?: any;
+  mobileConfigClasses?: string;
 }
 
 export function PageSection(props: Props) {
-  const { backgroundImage, children }  = props
-  console.log(backgroundImage)
+  const { backgroundImage, children, mobileConfigClasses }  = props
+
    return (
-    <section className={`flex bg-center bg-cover bg-no-repeat bg-no-repeat h-[70vh] items-center`} style={{backgroundImage: `url(${backgroundImage})`}}>
+    <section className={`flex bg-center bg-cover bg-no-repeat bg-no-repeat h-[70vh] items-center ${mobileConfigClasses}`} style={{backgroundImage: `url(${backgroundImage})`}}>
       { children }
     </section>
    )
