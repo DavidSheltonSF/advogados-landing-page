480d5c3388fc64d86f0f56a245cdbc31bc5bb055
DavidSheltonSF <david.faria012@gmail.com>
2025-10-03 15:46:16 -0300

    refactor(components): add properties to ProfessionalCard to enhance reuse

--- src/app/components/professionalCard/index.tsx
+++ src/app/components/professionalCard/index.tsx
@@ -1,26 +1,30 @@
+import { reduceText } from "@/app/utils/reduceText";
+
 interface Props {
-  photo: string
+  photo: string;
+  name: string;
+  description: string;
+  specialization: string;
+  experience: number;
 }
 
-
 export function ProfessionalCard(props: Props) {
-  const { photo } = props
-
+  const { photo, name, description, specialization, experience } = props
   return (
-    <article className="flex flex-col items-center w-88 h-128 rounded-2xl bg-[#f6f6f6] overflow-hidden p-5 shadow-md">
+    <article className="flex flex-col animate-pulse space-x-4 items-center w-88 h-128 rounded-2xl bg-[#f6f6f6] overflow-hidden p-5 shadow-md">
       <header className="h-[52%] w-full">
         <div className="size-full rounded-xl overflow-hidden">
           <img className="h-full w-full object-cover object-[50%_8%]" src={photo} alt="" />
         </div>
       </header>
       <main className=" h-[40%] flex flex-col items-center">
-        <h1 className="mt-5 font-bold text-black text-xl">Maria José</h1>
-        <p className="mt-2 text-black text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam magni accusantium ut incidunt reprehenderit odio beatae? Corrupti esse</p>
+        <h1 className="mt-5 font-bold text-black text-xl">{name}</h1>
+        <p className="mt-2 text-black text-center">{reduceText(description)}</p>
       </main>
       <footer className="h-[12%] w-full flex items-center justify-between">
-        <span className="text-black bg-blue-200 rounded-full content-center p-2 h-fit">Trabalhista</span>
+        <span className="text-black bg-blue-200 rounded-full content-center p-2 h-fit">{specialization}</span>
         
-        <span className="text-black">5 anos de experiência</span>
+        <span className="text-black">{experience} anos de experiência</span>
       </footer>
   
     </article>
