d4198c898034eea2acf73bbc155a6f8eab134671
DavidSheltonSF <david.faria012@gmail.com>
2025-10-03 19:23:15 -0300

    chore: enhance website preview and open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,17 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Hiath & Sales Adv",
   description: "Escritório de Advocacia - advogados de família e trabalhista",
-  icons: 'logo-with-background.svg',
+  icons: {
+    icon: '/logo-with-background.svg'
+  },
   openGraph: {
-    images: 'bg-image-logo-with-name-blue.png',
+    images: [
+      {
+        url: 'https://advogados-landing-page.onrender.com/website-preview.png',
+        width:1200,
+        height:630
+      }
+    ],
     title: "Hiath & Sales Adv",
     description: "Escritório de Advocacia - advogados de família e trabalhista",
     siteName: "Hiath & Sales Adv",
