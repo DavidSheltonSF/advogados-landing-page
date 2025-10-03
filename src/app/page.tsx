c8a4b2bca5dee489e1ddc8254ea1bac976b2ef0a
DavidSheltonSF <david.faria012@gmail.com>
2025-10-03 15:46:35 -0300

    refactor: enhance code readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,20 +3,31 @@ import { PageSection } from "./components/pageSection";
 import { ProfessionalCard } from "./components/professionalCard";
 
 export default function Home() {
+
+  const professionals = {
+    maria: {
+      photo: '/photos/maria-photo.png',
+      name: "Maria José",
+      description: "Lorem ipsum dolor sit met consectetur, adipisicing elit. Distinctio quaerat harum illum consequatur impedit voluptatum minus aut, mollitia laudantium nemo maiores nam totam aperiam velit asperiores, non quos recusandae.",
+      specialization: "Trabalhista",
+      experience: 5
+    },
+     adriana: {
+      photo: '/photos/adriana-photo.png',
+      name: "Adriana Hiath",
+      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, maiores tempora perferendis architecto nam minus quo quia consectetur totam et sequi possimus reiciendis, nemo culpa quos consequuntur laborum rerum sunt.",
+      specialization: "Família",
+      experience: 5
+    }
+  }
+
   return (
     <div className="flex flex-col">
-
-      <PageSection backgroundImage="/bg-image-logo-with-name.png">
-        <div className="flex flex-col gap-2 w-[24%] ml-16">
-          <h1 className="text-5xl">Hiath & Sales</h1>
-          <p className="text-3xl">Advocacia</p>
-          <p className="text-xl">Lorem ipsum dolor sit met consectetur, adipisicing elit. Distinctio quaerat harum illum consequatur impedit voluptatum minus aut, mollitia laudantium libero, nemo maiores nam totam aperiam velit asperiores, non quos recusandae.</p>
-        </div>
-      </PageSection>
+      <PageSection backgroundImage="/bg-image-logo-with-name.png"/>
       <PageSection backgroundImage="/bg-image-white.png">
          <div className="flex items-center justify-center gap-36 w-full h-full">
-          <ProfessionalCard photo="photos/maria-photo.png"/>
-          <ProfessionalCard photo="photos/adriana-photo.png"/>
+          <ProfessionalCard {...professionals['maria']}/>
+          <ProfessionalCard {...professionals['adriana']}/>
          </div>
       </PageSection>
       <div className="bg-red-300 h-[70vh]">
