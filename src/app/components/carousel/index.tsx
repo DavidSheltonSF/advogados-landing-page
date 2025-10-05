31ad248d438d707dc910c969eab0b2af7a099ef5
DavidSheltonSF <david.faria012@gmail.com>
2025-10-05 17:09:53 -0300

    feat(components): add Carousel

---
+++ src/app/components/carousel/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { useState } from "react";
+
+export function Carousel() {
+  const [current, setCurrent] = useState(0);
+  const images = [
+    'carousel/trabalhista.png',
+    'carousel/familia.png'
+  ]
+
+  const nextSlide = () => {
+    if(current < images.length - 1) {
+      setCurrent(current + 1)
+    }
+  }
+
+  const prevSlide = () => {
+     if(current > 0) {
+      setCurrent(current - 1)
+    }
+  }
+
+
+  return (
+    <div className="flex justify-center items-center relative bg-white w-160 h-160 overflow-hidden ">
+      <div className="overflow-hidden rounded-xl">
+        <div className="flex w-120 h-120 rounded-xl  transition-transform  duration-500" style={{transform: `translateX(-${current * 100}%)`}}>
+        {images.map((img, index) => {
+          return <img className="w-full flex-shrink-0" key={index} src={img} alt=""/>
+        })}
+      </div>
+      </div>
+      <button onClick={prevSlide} className="absolute top-1/2 left-3 text-blue-400">CLICK</button>
+      <button onClick={nextSlide} className="absolute top-1/2 right-3 text-white bg-blue-400">CLICK</button>
+    </div>
+  )
+}
