e34bf286e126d72a94231d607ab091f6b84b7c33
DavidSheltonSF <david.faria012@gmail.com>
2025-10-05 23:03:13 -0300

    feat(carousel): make button to appear disabled when necessary

--- src/app/components/carousel/index.tsx
+++ src/app/components/carousel/index.tsx
@@ -1,8 +1,17 @@
 'use client'
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function Carousel() {
   const [current, setCurrent] = useState(0);
+  let rightButton: any = undefined;
+  let leftButton: any = undefined;
+ 
+  // Access document after the browser is loaded
+  useEffect(() => {
+    rightButton = document.querySelector('.rightButton');
+    leftButton = document.querySelector('.leftButton');
+  }, [])
+
   let startX = 0;
   const images = [
     'carousel/trabalhista.png',
@@ -13,12 +22,23 @@ export function Carousel() {
   const nextSlide = () => {
     if(current < images.length - 1) {
       setCurrent(current + 1)
-    }
+      leftButton?.classList.remove('disabledElement')
+
+      if(current === images.length - 2) {
+        rightButton?.classList.add('disabledElement')
+      }
+    } 
   }
 
   const prevSlide = () => {
      if(current > 0) {
       setCurrent(current - 1)
+
+      rightButton?.classList.remove('disabledElement')
+
+      if(current === images.length - 2) {
+        leftButton?.classList.add('disabledElement')
+      }
     }
   }
 
@@ -38,19 +58,19 @@ export function Carousel() {
 
 
   return (
-    <div className="flex justify-center items-center relative w-180 h-160 overflow-hidden ">
+    <div className="flex justify-center items-center relative w-180 h-160 max-md:w-180 max-md:h-210 overflow-hidden ">
       <div onTouchStart={touchStart}  onTouchEnd={touched} className="overflow-hidden rounded-xl">
-        <div className="flex w-120 h-120 rounded-xl  transition-transform  duration-500" style={{transform: `translateX(-${current * 100}%)`}}>
+        <div className="flex w-120 h-120 max-md:w-130 max-md:h-130 max-sm:w-80 max-sm:h-90 rounded-xl transition-transform duration-500" style={{transform: `translateX(-${current * 100}%)`}}>
         {images.map((img, index) => {
           return <img className="w-full flex-shrink-0" key={index} src={img} alt=""/>
         })}
       </div>
       </div>
-      <button onClick={prevSlide} className="absolute top-1/2 left-1 text-blue-400 bg-white rounded-xl">
-        <img className="w-20" src="icons/arrow-left.svg" alt="" />
+      <button onClick={prevSlide} className="absolute top-1/2 left-1 text-blue-400 bg-white max-md:bg-white/60 rounded-xl leftButton">
+        <img className="w-20 max-md:w-15" src="icons/arrow-left.svg" alt="" />
       </button>
-      <button onClick={nextSlide} className="absolute top-1/2 right-1 text-white bg-blue-400 bg-white rounded-xl">
-        <img className="w-20" src="icons/arrow-right.svg" alt="" />
+      <button onClick={nextSlide} className="absolute top-1/2 right-1 text-white bg-blue-400 bg-white max-md:bg-white/60 rounded-xl rightButton">
+        <img className="w-20 max-md:w-15" src="icons/arrow-right.svg" alt="" />
       </button>
     </div>
   )
