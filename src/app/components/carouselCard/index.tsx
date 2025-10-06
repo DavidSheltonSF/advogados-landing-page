b4245abda07cbe8ae96df00bccc242c9c56ffb40
DavidSheltonSF <david.faria012@gmail.com>
2025-10-06 17:38:48 -0300

    feat(components) add CarouselCard

---
+++ src/app/components/carouselCard/index.tsx
@@ -0,0 +1,24 @@
+interface Props {
+  key: any
+  title: string;
+  image: string;
+  items: string[];
+}
+
+export function CarouselCard(props: Props) {
+  const {title, image, items} = props;
+  return (
+    <div className="relative w-full shrink-0">
+      <div className="flex justify-center absolute top-[50%] translate-y-[-50%] w-full h-[90%] bg-black/80">
+        <div className="flex w-[70%] h-full  flex-col ">
+          <h1 className="my-8 max-sm:my-4 mx-auto font-bold text-4xl text-secondary-color">{title}</h1>
+        <ul className="flex flex-col gap-1 text-secondary-color text-lg max-sm:gap-0 max-sm:text-sm decoration list-disc">
+          {items.slice(0,8).map((item, index) => <li key={`text-idem${index}`}>{item}</li>)}
+        </ul>
+
+        </div>
+      </div>
+      <img className="w-full"  src={image} alt=""/>
+    </div>
+  )
+}
