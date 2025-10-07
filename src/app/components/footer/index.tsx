4a74fd7eeaf238a3cf47ba2dc1a0d694cdee0986
DavidSheltonSF <david.faria012@gmail.com>
2025-10-07 19:56:43 -0300

    refactor(footer): increase footer height for enhancing layout

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -3,10 +3,10 @@ import { PageContainer } from "../pageContainer";
 
 export function Footer(){
   return (
-    <footer className="flex items-center h-[20vh] bg-black w-full">
-       <PageContainer extraClasses="flex flex-col h-[90%] items-center gap-6">
+    <footer className="flex items-center h-[30vh] bg-black w-full">
+       <PageContainer extraClasses="flex flex-col h-[90%] items-center gap-6 p">
           <div className="mt-8">
-            <h3 className="text-2xl font-bold">Siga-nos no nosso Instagram</h3>
+            <h3 className="text-2xl font-bold max-sm:text-center">Siga-nos no nosso Instagram</h3>
           </div>
           <div className="flex gap-8">
             <a 
