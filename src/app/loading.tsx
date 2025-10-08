3715f0cc3b4c23c493d70fb032fa87a7910a1359
DavidSheltonSF <david.faria012@gmail.com>
2025-10-08 14:32:25 -0300

    feat(loading): add loading animation to the page

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="bg-primary flex items-center justify-center h-[100vh] w-[100vw]">
+      <img className="min-lg:w-150" src="loading.gif" alt="" />
+    </div>
+  )
+}
