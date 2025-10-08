a4b4890b345f9760a314d067f8b9ee22035af0ed
DavidSheltonSF <david.faria012@gmail.com>
2025-10-08 16:00:33 -0300

    feat: add custom fonts; Poppins for titles and Lato for other texts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,18 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {Poppins, Lato} from "next/font/google"
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  variable: '--font-poppins'
+});
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-lato'
+});
 
 export const metadata: Metadata = {
   title: "Hiath & Sales Adv",
@@ -30,7 +42,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${poppins.variable} ${lato.variable}`}>
       <body
         className={`antialiased`}
       >
