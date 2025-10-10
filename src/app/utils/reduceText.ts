09a4fd4eb6653b2cd9c6cc60f5eebc487eca82ec
DavidSheltonSF <david.faria012@gmail.com>
2025-10-10 18:51:31 -0300

    refactor(utils): add numberOfWords param to reduceText, for more flexibility

--- src/app/utils/reduceText.ts
+++ src/app/utils/reduceText.ts
@@ -1,8 +1,8 @@
-export function reduceText(text:string) {
+export function reduceText(text:string, numberOfWords: number = 20) {
 
   const words = text.split(' ');
 
-  if(words.length >= 20) {
+  if(words.length >= numberOfWords) {
     const reducedWords = words.slice(0, 20);
     let reducedText = reducedWords.join(' ');
     reducedText = reducedText + '...';
