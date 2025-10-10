2bf96c308c553571be3341af7600cbcce2816023
DavidSheltonSF <david.faria012@gmail.com>
2025-10-10 19:10:49 -0300

    refactor(components): remove minimum height from PageSection component

--- src/app/components/pageSection/index.tsx
+++ src/app/components/pageSection/index.tsx
@@ -11,7 +11,7 @@ export function PageSection(props: Props) {
   const { backgroundImage, children, extraClasses }  = props
 
    return (
-    <section className={`flex bg-center bg-cover bg-no-repeat bg-no-repeat h-auto min-h-[70vh] items-center justify-center ${extraClasses}`} style={{backgroundImage: `url(${backgroundImage})`}}>
+    <section className={`flex bg-center bg-cover bg-no-repeat bg-no-repeat h-auto items-center justify-center ${extraClasses}`} style={{backgroundImage: `url(${backgroundImage})`}}>
       { children }
     </section>
    )
