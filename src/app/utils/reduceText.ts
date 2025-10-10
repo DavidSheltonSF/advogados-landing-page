1d3355c8db61ec105b95dd20780eb76ad060fd5d
DavidSheltonSF <david.faria012@gmail.com>
2025-10-10 18:55:40 -0300

    fkix(utils): make it slice the text using the numberOfWords param, but only if the length of the text is grater than the quantity of words

--- src/app/utils/reduceText.ts
+++ src/app/utils/reduceText.ts
@@ -2,8 +2,8 @@ export function reduceText(text:string, numberOfWords: number = 20) {
 
   const words = text.split(' ');
 
-  if(words.length >= numberOfWords) {
-    const reducedWords = words.slice(0, 20);
+  if(words.length > numberOfWords) {
+    const reducedWords = words.slice(0, numberOfWords);
     let reducedText = reducedWords.join(' ');
     reducedText = reducedText + '...';
     return reducedText
