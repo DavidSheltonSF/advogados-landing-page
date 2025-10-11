121366ccf35e59b4b2f3e02548f41e1a4e06980a
DavidSheltonSF <david.faria012@gmail.com>
2025-10-11 14:56:02 -0300

    refactor(global loading): remove global loading

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function Loading() {
-  return (
-    <div className="bg-primary flex items-center justify-center h-[100vh] w-[100vw]">
-      <img className="min-lg:w-150" src="loading.gif" alt="" />
-    </div>
-  )
-}
