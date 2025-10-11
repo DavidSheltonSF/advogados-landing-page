6f28e80fc319c50b52832da53d7df5f2f0311ff5
DavidSheltonSF <david.faria012@gmail.com>
2025-10-11 13:55:47 -0300

    refactor: fix footer text color and add section to Icon Credits

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -4,19 +4,34 @@ import { PageContainer } from "../pageContainer";
 
 export function Footer(){
   return (
-    <footer className="flex items-center h-[20vh] max-sm:h-[30vh] bg-black w-full">
-       <PageContainer extraClasses="flex flex-col h-[90%] items-center gap-6 p">
-          <div className="mt-8">
-            <h3 className="text-2xl font-bold max-sm:text-center">Siga-nos no nosso Instagram</h3>
+    <footer className="flex flex-col h-[40vh] max-sm:h-[50vh] bg-black w-full text-white">
+       <PageContainer extraClasses="flex flex-col items-center justify-center gap-6 p">
+          <div className="flex justify-center items-center mt-5">
+            <h3 className="text-2xl font-bold max-sm:text-center">Siga-nos nas redes sociais</h3>
           </div>
           <div className="flex gap-8">
             <a 
-          href="https://www.instagram.com/advocaciahiathesales/"
+          href="#"
+          target="_blank"
+          className="cursos-pointer"
+          >
+            <img className="w-10" src="icons/facebook-white-icon.png" alt="" />
+          </a>
+          <a 
+          href="#"
           target="_blank"
           className="cursos-pointer"
           >
             <img className="w-10" src="icons/instagram-white-icon.svg" alt="" />
           </a>
+          </div>
+        </PageContainer>
+        <PageContainer extraClasses="flex flex-col items-center justify-center gap-6">
+          <div>
+            <h3 className="text-xl font-bold max-sm:text-center">Icon Credits</h3>
+          </div>
+          <div>
+            <a href="https://www.flaticon.com/free-icons/law" title="law icons">Law icons created by Freepik - Flaticon</a>
           </div>
         </PageContainer>
     </footer>
