d92433f7919803e371685142473cacfde10b5b50
DavidSheltonSF <david.faria012@gmail.com>
2025-10-13 23:35:46 -0300

    chore:(next configs): add caching configs for images in /public

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,24 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true // porque o export é estático
+  },
+  async headers() {
+    return [
+      {
+        // para todos os arquivos estáticos em /public
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default nextConfig;
