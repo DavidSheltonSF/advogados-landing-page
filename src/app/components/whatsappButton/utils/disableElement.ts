d88e374f2b6fe846ce60175e1f2b3dc30798c09c
DavidSheltonSF <david.faria012@gmail.com>
2025-10-13 22:19:56 -0300

    refactor(utils): delete duplicated utilitary functions

--- src/app/components/whatsappButton/utils/disableElement.ts
+++
@@ -1,5 +0,0 @@
-export function disableElement(elementClass: string) {
-  const element = document.querySelector(elementClass);
-  element?.classList.remove('cursor-pointer');
-  element?.classList.add('disabledElement')
-}
--- src/app/components/whatsappButton/utils/enableElement.ts
+++
@@ -1,5 +0,0 @@
-export function enableElement(elementClass: string) {
-  const element = document.querySelector(elementClass);
-  element?.classList.add('cursor-pointer');
-  element?.classList.remove('disabledElement')
-}
