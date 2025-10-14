f9c14a01197e3a5fb8b46c039a3d11959e53c168
DavidSheltonSF <david.faria012@gmail.com>
2025-10-13 21:01:35 -0300

    style(utils): remove unnecessary blank lines

--- src/app/utils/reduceText.ts
+++ src/app/utils/reduceText.ts
@@ -1,13 +1,10 @@
 export function reduceText(text:string, wordsQuantity: number = 20) {
-
   const words = text.split(' ');
-
   if(words.length > wordsQuantity) {
     const reducedWords = words.slice(0, wordsQuantity);
     let reducedText = reducedWords.join(' ');
     reducedText = reducedText + '...';
     return reducedText
   }
-
   return text
 }
