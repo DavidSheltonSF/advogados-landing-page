db3912a03f299c85b5a6dab436a331eee935c583
DavidSheltonSF <david.faria012@gmail.com>
2025-10-14 15:04:21 -0300

    chore(next config): remove unknown keys and headers (they do not work on static apps directly)

--- next.config.ts
+++ next.config.ts
@@ -3,28 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  swcMinify: true,
-  browserList: [],
   compiler: {
     removeConsole: true,
   },
   images: {
     unoptimized: true // porque o export é estático
   },
-  async headers() {
-    return [
-      {
-        // para todos os arquivos estáticos em /public
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          }
-        ]
-      }
-    ]
-  }
 };
 
 export default nextConfig;
