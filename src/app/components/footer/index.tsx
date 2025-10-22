83cb1eaebb25670ec9821bb621a156bf64cb1868
DavidSheltonSF <david.faria012@gmail.com>
2025-10-22 15:52:11 -0300

    refactor(footer): remove icon credits  and reduce height.
    The website will use the client's own icon

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -4,7 +4,7 @@ import { PageContainer } from "../pageContainer";
 
 export function Footer(){
   return (
-    <footer className="flex flex-col h-[35vh] max-sm:h-[40vh] bg-black w-full text-white">
+    <footer className="flex flex-col h-[25vh] max-sm:h-[30vh] bg-black w-full text-white">
        <PageContainer extraClasses="flex flex-col items-center justify-center gap-6 p">
           <div className="flex justify-center items-center mt-5">
             <h3 className="text-2xl font-bold max-sm:text-center">Siga-nos nas redes sociais</h3>
@@ -26,14 +26,7 @@ export function Footer(){
           </a>
           </div>
         </PageContainer>
-        <PageContainer extraClasses="flex flex-col items-center justify-center gap-6">
-          <div>
-            <h3 className="text-xl font-bold max-sm:text-center">Icon Credits</h3>
-          </div>
-          <div>
-            <a href="https://www.flaticon.com/free-icons/law" title="law icons">Law icons created by Freepik - Flaticon</a>
-          </div>
-        </PageContainer>
+       
     </footer>
   )
 }
