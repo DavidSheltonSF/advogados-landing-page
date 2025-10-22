166dfe8c36b8536b41b66a783290d3a69a03dc98
DavidSheltonSF <david.faria012@gmail.com>
2025-10-22 20:02:45 -0300

    feat(ProfessionalCard): add flip feature and back side

--- src/app/components/professionalCard/index.tsx
+++ src/app/components/professionalCard/index.tsx
@@ -13,12 +13,25 @@ interface Props {
 }
 
 export function  ProfessionalCard(props: Props) {
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
+  const [flipped, setFlipped] = useState(false);
   useEffect(() => {
     setIsLoading(false);
   }, [])
 
+  function flipCard(e: any){
+    if(!flipped){
+      setFlipped(true);
+    }
+  }
 
+  function unFlipCard(){
+    if(flipped){
+      setFlipped(false);
+    }
+  }
+
+ 
   const { photo, name, description, specialization, experience } = props
   return (
    <>
@@ -27,20 +40,31 @@ export function  ProfessionalCard(props: Props) {
       <ProfessionalCardSkeleton/>
 
     :
-    <article className="flex flex-col items-center w-88 h-128 rounded-2xl bg-[#f6f6f6] p-5 shadow-md hover:-translate-y-5 transition duration-200">
-      <header className="h-[52%] w-full">
-        <div className="size-full rounded-xl overflow-hidden aspect-video">
-          <img className="object-cover w-full h-full object-top" src={photo} alt="" loading="lazy"/>
+    <article className={`relative professional-card flex flex-col items-center w-88 h-128 rounded-2xl bg-[#f6f6f6] shadow-md hover:-translate-y-5 transition duration-200 overflow-hidden ${flipped ? 'rotate-y-180' : ''}`} onClick={flipCard}>
+      <div className="relative flex flex-col items-center w-full h-full p-5">
+        <header className="h-[52%] w-full">
+          <div className="size-full rounded-xl overflow-hidden aspect-video">
+            <img className="object-cover w-full h-full object-top" src={photo} alt="" loading="lazy"/>
+          </div>
+        </header>
+        <main className="h-[40%] flex flex-col items-center">
+          <h1 className="mt-5 font-bold text-black text-xl">{name}</h1>
+          <p className="mt-2 text-black text-center">{reduceText(description, 27)}</p>
+        </main>
+        <footer className="h-[12%] w-full flex items-center justify-between">
+          <span className="text-black bg-blue-200 rounded-full content-center p-2 h-fit">{specialization}</span>
+          <span className="text-black">{experience} anos de experiência</span>
+        </footer>
+      </div>
+      <div className={`card-back absolute rotate-y-180 background-secondary-color h-full w-full ${!flipped ? 'hidden' : ''}`}>
+        <div className="relative w-full h-full flex flex-col items-center justify-center gap-4">
+          <div className="absolute bg-gray-200 hover:bg-gray-300 w-10 h-10 rounded-full right-[5%] top-[5%]" onClick={unFlipCard}></div>
+          <div className="rounded-full hover:bg-white/50 whatsapp-btn">
+            <img className="w-12 max-md:w-8" src="icons/whatsapp-white-icon.svg" alt="" loading="lazy"/>
+          </div>
+          <h1 className="font-bold text-xl">Fale com a Dra. {name}</h1>
         </div>
-      </header>
-      <main className="h-[40%] flex flex-col items-center">
-        <h1 className="mt-5 font-bold text-black text-xl">{name}</h1>
-        <p className="mt-2 text-black text-center">{reduceText(description, 27)}</p>
-      </main>
-      <footer className="h-[12%] w-full flex items-center justify-between">
-        <span className="text-black bg-blue-200 rounded-full content-center p-2 h-fit">{specialization}</span>
-        <span className="text-black">{experience} anos de experiência</span>
-      </footer>
+      </div>
     </article>
      }
    </>
