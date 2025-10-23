e81c11ce261cfc79b7fa3bc6050b88c81fb60fab
DavidSheltonSF <david.faria012@gmail.com>
2025-10-23 14:24:21 -0300

    refactor(professional cards): remove 'Anos de experiência' spam

--- src/app/components/professionalCard/index.tsx
+++ src/app/components/professionalCard/index.tsx
@@ -41,19 +41,18 @@ export function  ProfessionalCard(props: Props) {
       <ProfessionalCardSkeleton/>
 
     :
-    <article className={`relative professional-card flex flex-col items-center w-88 h-140 rounded-2xl bg-[#f6f6f6] shadow-md hover:-translate-y-5 transition duration-200 overflow-hidden ${flipped ? 'rotate-y-180' : ''}`} onClick={flipCard}>
+    <article className={`relative professional-card flex flex-col items-center w-88 h-132 rounded-2xl bg-[#f6f6f6] shadow-md hover:-translate-y-5 transition duration-200 overflow-hidden ${flipped ? 'rotate-y-180' : ''}`} onClick={flipCard}>
       <div className="relative flex flex-col items-center w-full h-full p-5">
         <header className="h-[52%] w-full">
           <div className="size-full rounded-xl overflow-hidden aspect-video">
             <img className="object-cover w-full h-full object-top" src={photo} alt="" loading="lazy"/>
           </div>
         </header>
-        <main className="h-[32%] flex flex-col items-center">
+        <main className="h-[36%] flex flex-col items-center">
           <h1 className="mt-5 font-bold text-black text-xl">{name}</h1>
           <p className="mt-2 text-black text-center">{reduceText(description, 27)}</p>
         </main>
-        <footer className="h-[12%] w-full flex flex-col items-center gap-4">
-           <span className="text-center text-black w-full rounded-xl background-secondary-color">{experience} anos de experiência</span>
+        <footer className="h-[12%] w-full flex flex-col items-center justify-center gap-4">
           <div className="flex gap-4">
             {
               specializations.map(specialization => {
